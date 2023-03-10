ef69c1594fc5a1bbdd872735bf46f80f7c74c606
VasiliiOrlov <ov_prog@mail.ru>
2023-03-10 11:32:09 +0300

    Регистрация пользователя [#296069] fix3

--- src/main/java/ru/job4j/accidents/service/JpaUserService.java
+++ src/main/java/ru/job4j/accidents/service/JpaUserService.java
@@ -1,16 +1,15 @@
 package ru.job4j.accidents.service;
 
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.JpaUserRepository;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class JpaUserService {
-    private static final Logger LOG = LoggerFactory.getLogger(JpaUserService.class.getName());
     private final JpaUserRepository userRepository;
     public boolean save(User user) {
         boolean rsl = false;
@@ -18,7 +17,7 @@ public class JpaUserService {
             userRepository.save(user);
             rsl = true;
         } catch (Exception e) {
-            LOG.error("Ошибка при регистрации");
+            log.error("Ошибка при регистрации");
         }
         return rsl;
     }
